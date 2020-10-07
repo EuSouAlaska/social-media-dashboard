a12d7e6b7e68583f70a521b71cd4c52ff0342924
EuSouAlaska <51723157+EuSouAlaska@users.noreply.github.com>
2020-10-06 21:28:51 -0300

    meu

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-var themeSwitcher = document.getElementById("switch-theme");
-var currentTheme = "light";
-
-themeSwitcher.addEventListener(
-  "click",
-  function () {
-    var lightTheme = "./light-theme.css";
-    var darkTheme = "./dark-theme.css";
-
-    var oldCss = document.getElementsByTagName("link").item(2);
-
-    var newCss = document.createElement("link");
-    newCss.setAttribute("rel", "stylesheet");
-    newCss.setAttribute("type", "text/css");
-
-    if (currentTheme === "light") {
-      newCss.setAttribute("href", darkTheme);
-      currentTheme = "dark";
-    } else if (currentTheme === "dark") {
-      newCss.setAttribute("href", lightTheme);
-      currentTheme = "light";
-    }
-
-    document.getElementsByTagName("head").item(0).replaceChild(newCss, oldCss);
-  },
-  false
-);
-
-/* SCRIPT BY https://github.com/winstoncodes */
+const themeSwitcher = document.getElementById("switch-theme");
+function troca() {
+  const selectBody = document.body.classList.toggle("ativo");
+}
+themeSwitcher.addEventListener("click", troca);
